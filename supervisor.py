93b7675c2456070a7fe2ca5ff31e2cc9e1348b35
RobertMinter <39847333+RobertMinter@users.noreply.github.com>
2018-12-02 15:49:41 -0500

    Add files via upload

---
+++ supervisor.py
@@ -0,0 +1,10 @@
+def UpdateConfiguration():
+	
+	#read message from main process
+	
+	for i in list_of_nodes:
+		#depoly new config to the node
+
+def StartNode():
+
+	#at address tell bro to start a new node
