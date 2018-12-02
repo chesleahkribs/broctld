7c4532060a4b4d5f3d380341e46e0b0ef462a981
KiraJ42 <spockian42@gmail.com>
2018-12-02 13:32:46 -0500

    Update broctl_shell.py

--- broctl_shell.py
+++ broctl_shell.py
@@ -1,8 +1,44 @@
+import brctld
+
+
 def spawn_bro():
-    print "this should tell supervisor process to spawn bro process"
-  
-def start_main():
-    print
-  
-def start_supervisor(): 
-         
+	print "This should tell supervisor process to span bro process"
+
+def start_supervisor():
+	print "Start supervisor process"
+
+def update():
+	print "end main process"
+
+def print_help_menu():
+	print "quit\t\t-exit shell"
+	print "exit\t\t-exit shell"
+	print "start\t\t-start supervisor process"
+	print "spawn\t\t-create new bro process"
+
+
+
+if __name__ == "__main__":
+	prompt =  "[BroControl] "
+
+	print '\nWelcome to BroControl 1.4\n\nType "help" for help.\n\n'
+
+	brctld.start_main();
+
+	while True:
+		n = raw_input(prompt)
+		input = n.strip()
+
+		if input == "exit":
+			print "GOODBYE"
+			break
+		elif input == "help":
+			print_help_menu()
+		elif input == "update":
+			update()
+		elif input == "start":
+			start_supervisor()
+		elif input == "spawn":
+			spawn_bro()
+		else:
+			print "Invalid Command"
